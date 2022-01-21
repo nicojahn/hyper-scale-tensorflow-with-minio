85992f8cc03d58de1b61ea9eff0dd3f126534779
Nico Jahn <dev@nicojahn.com>
2022-01-21 19:41:39 +0100

    Fixed bugs, added pre-commit for better code quality, added .env file, updated README.md

--- MinIO_Tensorflow.py
+++ MinIO_Tensorflow.py
@@ -1,43 +1,45 @@
+# -*- coding: utf-8 -*-
 # Copyright MinIO 2020
-
-
+import math
 import os
 import random
 import tarfile
-import timeit
+import time
 from datetime import datetime
 
-import math
 import tensorflow as tf
 import tensorflow_hub as hub
+import tensorflow_io as tfio  # pylint: disable=W0611
+from dotenv import load_dotenv
 from minio import Minio
-from minio.error import ResponseError
+from minio.error import InvalidResponseError
 from tensorflow import keras
 
-random_seed = 44
-batch_size = 128
-datasets_bucket = "datasets"
-preprocessed_data_folder = "preprocessed-data"
-tf_record_file_size = 500
-# How to access MinIO
-minio_address = "localhost:9000"
-minio_access_key = "minioadmin"
-minio_secret_key = "minioadmin"
+RANDOM_SEED = 44
+BATCH_SIZE = 128
+PREPROCESSED_DATA_FOLDER = "preprocessed-data"
+TF_RECORD_FILE_SIZE = 500
 
-minioClient = Minio(minio_address,
-                    access_key=minio_access_key,
-                    secret_key=minio_secret_key,
-                    secure=False)
+load_dotenv()
+S3_ENDPOINT = os.getenv("S3_ENDPOINT")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+S3_USE_HTTPS = bool(int(os.getenv("S3_USE_HTTPS")))
+DATASET_BUCKET = os.getenv("DATASET_BUCKET")
+
+minioClient = Minio(
+    S3_ENDPOINT,
+    access_key=AWS_ACCESS_KEY_ID,
+    secret_key=AWS_SECRET_ACCESS_KEY,
+    secure=S3_USE_HTTPS,
+)
 
 try:
-    minioClient.fget_object(
-        datasets_bucket,
-        "aclImdb_v1.tar.gz",
-        "/tmp/dataset.tar.gz")
-except ResponseError as err:
+    minioClient.fget_object(DATASET_BUCKET, "aclImdb_v1.tar.gz", "/tmp/dataset.tar.gz")
+except InvalidResponseError as err:  # pylint: disable=W0621
     print(err)
 
-extract_folder = f"/tmp/{datasets_bucket}/"
+extract_folder = f"/tmp/{DATASET_BUCKET}/"
 
 with tarfile.open("/tmp/dataset.tar.gz", "r:gz") as tar:
     tar.extractall(path=extract_folder)
@@ -62,18 +64,12 @@ for dir_name in dirs_to_read:
         with open(os.path.join(extract_folder, dir_name, filename), "r") as f:
             content = f.read()
             if dataset == "train":
-                train.append({
-                    "text": content,
-                    "label": label
-                })
+                train.append({"text": content, "label": label})
             elif dataset == "test":
-                test.append({
-                    "text": content,
-                    "label": label
-                })
+                test.append({"text": content, "label": label})
 
-random.Random(random_seed).shuffle(train)
-random.Random(random_seed).shuffle(test)
+random.Random(RANDOM_SEED).shuffle(train)
+random.Random(RANDOM_SEED).shuffle(test)
 
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
 
@@ -87,10 +83,17 @@ def _label_feature(value):
 
 
 def encode_label(label):
+    ret_val = None
     if label == "pos":
-        return tf.constant([1, 0])
+        ret_val = tf.constant([1, 0])
     elif label == "neg":
-        return tf.constant([0, 1])
+        ret_val = tf.constant([0, 1])
+
+    if ret_val is None:
+        raise ValueError(
+            f"'{label}' is not a valid label (only 'pos' or 'neg' are allowed)"
+        )
+    return ret_val
 
 
 def serialize_example(label, sentence_tensor):
@@ -103,60 +106,66 @@ def serialize_example(label, sentence_tensor):
 
 
 def process_examples(records, prefix=""):
-    starttime = timeit.default_timer()
+    starttime = time.time()
     total_training = len(records)
     print(f"Total of {total_training} elements")
-    total_batches = math.floor(total_training / tf_record_file_size)
-    if total_training % tf_record_file_size != 0:
+    total_batches = math.floor(total_training / TF_RECORD_FILE_SIZE)
+    if total_training % TF_RECORD_FILE_SIZE != 0:
         total_batches += 1
-    print(f"Total of {total_batches} files of {tf_record_file_size} records")
+    print(f"Total of {total_batches} files of {TF_RECORD_FILE_SIZE} records")
 
     counter = 0
     file_counter = 0
     buffer = []
     file_list = []
-    for i in range(len(records)):
+    for elem in records:
         counter += 1
 
-        sentence_embedding = embed([records[i]["text"]])
-        label_encoded = encode_label(records[i]["label"])
+        sentence_embedding = embed([elem["text"]])
+        label_encoded = encode_label(elem["label"])
         record = serialize_example(label_encoded, sentence_embedding)
         buffer.append(record)
 
-        if counter >= tf_record_file_size:
+        if counter >= TF_RECORD_FILE_SIZE:
             print(f"Records in buffer {len(buffer)}")
             # save this buffer of examples as a file to MinIO
             counter = 0
             file_counter += 1
             file_name = f"{prefix}_file{file_counter}.tfrecord"
-            with open(file_name, "w+") as f:
-                with tf.io.TFRecordWriter(f.name, options="GZIP") as writer:
+            with open(file_name, "w+") as file:
+                with tf.io.TFRecordWriter(file.name) as writer:
                     for example in buffer:
                         writer.write(example.SerializeToString())
             print(f"file size {os.stat(file_name).st_size}")
             try:
-                minioClient.fput_object(datasets_bucket, f"{preprocessed_data_folder}/{file_name}", file_name)
-            except ResponseError as err:
+                minioClient.fput_object(
+                    DATASET_BUCKET, f"{PREPROCESSED_DATA_FOLDER}/{file_name}", file_name
+                )
+            except InvalidResponseError as err:  # pylint: disable=W0621
                 print(err)
             file_list.append(file_name)
             os.remove(file_name)
             buffer = []
-            print(f"Done with batch {file_counter}/{total_batches} - {timeit.default_timer() - starttime}")
+            print(
+                f"Done with batch {file_counter}/{total_batches} - {time.time() - starttime}"
+            )
     if len(buffer) > 0:
         file_counter += 1
-        file_name = f"file{file_counter}.tfrecord"
-        with open(file_name, "w+") as f:
-            with tf.io.TFRecordWriter(f.name) as writer:
+        file_name = f"{prefix}_file{file_counter}.tfrecord"
+        with open(file_name, "w+") as file:
+            with tf.io.TFRecordWriter(file.name) as writer:
                 for example in buffer:
                     writer.write(example.SerializeToString())
         try:
-            minioClient.fput_object(datasets_bucket, f"{preprocessed_data_folder}/{file_name}", file_name)
-        except ResponseError as err:
+            minioClient.fput_object(
+                DATASET_BUCKET, f"{PREPROCESSED_DATA_FOLDER}/{file_name}", file_name
+            )
+        except InvalidResponseError as err:  # pylint: disable=W0621
             print(err)
         file_list.append(file_name)
         os.remove(file_name)
         buffer = []
-    print("total time is :", timeit.default_timer() - starttime)
+    print("total time is :", time.time() - starttime)
     return file_list
 
 
@@ -165,26 +174,23 @@ process_examples(test, prefix="test")
 print("Done!")
 
 # List all training tfrecord files
-objects = minioClient.list_objects_v2(datasets_bucket, prefix="train")
+objects = minioClient.list_objects(
+    DATASET_BUCKET, prefix=f"{PREPROCESSED_DATA_FOLDER}/train"
+)
 training_files_list = []
 for obj in objects:
     training_files_list.append(obj.object_name)
 
 # List all testing tfrecord files
-objects = minioClient.list_objects_v2(datasets_bucket, prefix="test")
+objects = minioClient.list_objects(
+    DATASET_BUCKET, prefix=f"{PREPROCESSED_DATA_FOLDER}/test"
+)
 testing_files_list = []
 for obj in objects:
     testing_files_list.append(obj.object_name)
 
-os.environ["AWS_ACCESS_KEY_ID"] = minio_access_key
-os.environ["AWS_SECRET_ACCESS_KEY"] = minio_secret_key
-os.environ["AWS_REGION"] = "us-east-1"
-os.environ["S3_ENDPOINT"] = minio_address
-os.environ["S3_USE_HTTPS"] = "0"
-os.environ["S3_VERIFY_SSL"] = "0"
-
-all_training_filenames = [f"s3://datasets/{f}" for f in training_files_list]
-testing_filenames = [f"s3://datasets/{f}" for f in testing_files_list]
+all_training_filenames = [f"s3://{DATASET_BUCKET}/{f}" for f in training_files_list]
+testing_filenames = [f"s3://{DATASET_BUCKET}/{f}" for f in testing_files_list]
 
 total_train_data_files = math.floor(len(all_training_filenames) * 0.9)
 if total_train_data_files == len(all_training_filenames):
@@ -192,19 +198,23 @@ if total_train_data_files == len(all_training_filenames):
 training_files = all_training_filenames[0:total_train_data_files]
 validation_files = all_training_filenames[total_train_data_files:]
 
+# Due to an uncatched TypeError caused by urllib3(==1.26.8) on exit, we call 'del' explicitly
+# Exception ignored in: <function Minio.__del__ at 0x15e67cca0>
+del minioClient
+
 # Now let's create the `tf.data` datasets:
 
 AUTO = tf.data.experimental.AUTOTUNE
 ignore_order = tf.data.Options()
 ignore_order.experimental_deterministic = False
 
-dataset = tf.data.TFRecordDataset(training_files, num_parallel_reads=AUTO, compression_type="GZIP")
+dataset = tf.data.TFRecordDataset(training_files, num_parallel_reads=AUTO)
 dataset = dataset.with_options(ignore_order)
 
-validation = tf.data.TFRecordDataset(validation_files, num_parallel_reads=AUTO, compression_type="GZIP")
+validation = tf.data.TFRecordDataset(validation_files, num_parallel_reads=AUTO)
 validation = validation.with_options(ignore_order)
 
-testing_dataset = tf.data.TFRecordDataset(testing_filenames, num_parallel_reads=AUTO, compression_type="GZIP")
+testing_dataset = tf.data.TFRecordDataset(testing_filenames, num_parallel_reads=AUTO)
 testing_dataset = testing_dataset.with_options(ignore_order)
 
 
@@ -222,32 +232,17 @@ def decode_fn(record_bytes):
 
 model = keras.Sequential()
 
-model.add(
-    keras.layers.Dense(
-        units=256,
-        input_shape=(1, 512),
-        activation="relu"
-    )
-)
-model.add(
-    keras.layers.Dropout(rate=0.5)
-)
+model.add(keras.layers.Dense(units=256, input_shape=(1, 512), activation="relu"))
+model.add(keras.layers.Dropout(rate=0.5))
 
-model.add(
-    keras.layers.Dense(
-        units=16,
-        activation="relu"
-    )
-)
-model.add(
-    keras.layers.Dropout(rate=0.5)
-)
+model.add(keras.layers.Dense(units=16, activation="relu"))
+model.add(keras.layers.Dropout(rate=0.5))
 
 model.add(keras.layers.Dense(2, activation="softmax"))
 model.compile(
     loss="categorical_crossentropy",
     optimizer=keras.optimizers.Adam(0.001),
-    metrics=["accuracy"]
+    metrics=["accuracy"],
 )
 
 # ![Structure of our Deep Learning model](pic2.png)
@@ -255,34 +250,28 @@ model.compile(
 
 model.summary()
 
-# Let's prepare our datasets for the training stage by having them repeat
-# themselves a little and batch `128` items at a time
-
-
+# Let's prepare our datasets for the training stage by batching 'BATCH_SIZE' items at a time
 mapped_ds = dataset.map(decode_fn)
-mapped_ds = mapped_ds.repeat(5)
-mapped_ds = mapped_ds.batch(128)
+mapped_ds = mapped_ds.batch(BATCH_SIZE)
 
 mapped_validation = validation.map(decode_fn)
-mapped_validation = mapped_validation.repeat(5)
-mapped_validation = mapped_validation.batch(128)
+mapped_validation = mapped_validation.batch(BATCH_SIZE)
 
 testing_mapped_ds = testing_dataset.map(decode_fn)
-testing_mapped_ds = testing_mapped_ds.repeat(5)
-testing_mapped_ds = testing_mapped_ds.batch(128)
+testing_mapped_ds = testing_mapped_ds.batch(BATCH_SIZE)
 
-checkpoint_path = f"s3://{datasets_bucket}/checkpoints/cp.ckpt"
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
+checkpoint_path = f"s3://{DATASET_BUCKET}/checkpoints/cp.ckpt"
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path, save_weights_only=True, verbose=1
+)
 
-model_note = "256"
-logdir = f"s3://{datasets_bucket}/logs/imdb/{model_note}-" + datetime.now().strftime("%Y%m%d-%H%M%S")
+MODEL_NOTE = "256"
+logdir = f"s3://{DATASET_BUCKET}/logs/imdb/{MODEL_NOTE}-" + datetime.now().strftime(
+    "%Y%m%d-%H%M%S"
+)
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
 # Finally we will train the model:
-
-
 history = model.fit(
     mapped_ds,
     epochs=10,
@@ -291,9 +280,9 @@ history = model.fit(
 )
 
 # Now that we have our model, we want to save it to MinIO
-model.save(f"s3://{datasets_bucket}/imdb_sentiment_analysis")
-
-testing = model.evaluate(testing_mapped_ds)
+# Note: This might raise a warning
+#   (compare to: https://github.com/tensorflow/tensorboard/issues/48561)
+model.save(f"s3://{DATASET_BUCKET}/imdb_sentiment_analysis")
 
 samples = [
     "This movie sucks",
@@ -304,11 +293,15 @@ samples = [
     "I don't know what's the point of this movie, this movie sucks but the acting is great",
     "This is not a good movie",
 ]
-sample_embedded = embed(samples)
 
+print("Here are some examples:")
+sample_embedded = tf.expand_dims(embed(samples), axis=1)
 res = model.predict(sample_embedded)
-for s in range(len(samples)):
-    if res[s][0] > res[s][1]:
-        print(f"{samples[s]} - positive")
+for i, sample in enumerate(samples):
+    if res[i][0][0] > res[i][0][1]:
+        print(f"\t{sample} - positive")
     else:
-        print(f"{samples[s]} - negative")
+        print(f"\t{sample} - negative")
+
+testing = model.evaluate(testing_mapped_ds)
+print(f"Test set accuracy is at {testing[1]:.2f}")
--- README.md
+++ README.md
@@ -1,10 +1,32 @@
 # Hyper-Scale Machine Learning with MinIO and TensorFlow
 
-This is the code for the Blog Post: [Hyper-Scale Machine Learning with MinIO and TensorFlow](https://min.io)
+This is an update of the code for the Blog Post: [Hyper-Scale Machine Learning with MinIO and TensorFlow](https://min.io)
 
-This was built and tested using: 
-• Python 3.7
-• TensorFlow 2.2.0
-• minio-py 5.0.10
+This was built and tested using a macOS and Ubuntu system:
+* Python 3.9
+* TensorFlow 2.7.0
+* TensorBoard 2.8.0 (>=2.8.0, as S3 support is added via TF IO: https://github.com/tensorflow/tensorboard/issues/5480)
+* minio-py 7.1.2
+* and all the depencies in the 'requirements.txt'
+
+### How to use it
+* Install depencies
+```bash
+python3 -m pip install -r requirements.txt
+```
+
+* Specifiy the necessary environment variables in the '.env' file
+* Execute data pipeline, training and validation (Note: the data pipeline takes the longest)
+```bash
+python3 MinIO_Tensorflow.py
+```
+
+* Inspect the model via TensorBoard
+ * Please note, that exporting your credentials might be a security risk (even if it is just temporarily), thus we unset it afterwards
+```bash
+export $(grep -v '^#' .env | xargs)
+tensorboard --logdir s3://$DATASET_BUCKET/logs/
+unset $(grep -v '^#' .env | cut -d'=' -f1 | xargs)
+```
 
 Copyright MinIO 2020
